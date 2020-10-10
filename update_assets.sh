f2b91d752d8884187259365b7ff303041685df1c
Jo Shinonome <jo.shinonome@gmail.com>
2020-10-10 11:34:32 +0900

    update perspective

--- update_assets.sh
+++ update_assets.sh
@@ -6,7 +6,7 @@ npm update
 jsDir=assets/qview/js
 cssDir=assets/qview/css
 cp node_modules/@finos/perspective/dist/umd/perspective.inline* $jsDir
-cp node_modules/@finos/perspective-viewer/dist/umd/material* $cssDir
+cp node_modules/@finos/perspective-viewer/dist/umd/material*css $cssDir
 cp node_modules/@finos/perspective-viewer/dist/umd/perspective-viewer* $jssDir
 cp node_modules/@finos/perspective-viewer-d3fc/dist/umd/perspective-viewer-d3fc.js $jsDir
 cp node_modules/@finos/perspective-viewer-hypergrid/dist/umd/perspective-viewer-hypergrid.js $jsDir
