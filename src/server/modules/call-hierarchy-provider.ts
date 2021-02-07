f2d8d763dc072745314379cad69969502a340670
Jo Shinonome <jo.shinonome@gmail.com>
2021-02-07 16:30:10 +0900

    add comments

--- src/server/modules/call-hierarchy-provider.ts
+++ src/server/modules/call-hierarchy-provider.ts
@@ -22,6 +22,7 @@ export default class CallHierarchyProvider {
         if (word) {
             const symInfos = this.analyzer.getSymbolsByUri(params.textDocument.uri).filter(symInfo => symInfo.name === word.text);
             if (symInfos.length > 0 && symInfos[0].kind === SymbolKind.Function) {
+                // the first item should be itself
                 return [
                     {
                         kind: SymbolKind.Function,
@@ -40,10 +41,12 @@ export default class CallHierarchyProvider {
         const containerName = params.item.name as string;
         if (!containerName)
             return [];
+        // search for functions call this function
         const items = this.analyzer.getCallHierarchyItemByWord(containerName);
         return items.map(item => ({ from: item, fromRanges: [item.range] }));
     }
     onOutgoingCalls(params: CallHierarchyOutgoingCallsParams): CallHierarchyOutgoingCall[] {
+        // search what this function calls
         const globalId = this.analyzer.getGlobalIdByUriContainerName(params.item.uri, params.item.data as string);
         return globalId.map(id => this.analyzer.getCallHierarchyItemByWord(id)).flat(1)
             .map(item => ({ to: item, fromRanges: [item.range] }));
