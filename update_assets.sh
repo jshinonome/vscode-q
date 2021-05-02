d2100e3da5f02d34843528f1038d5290dde73872
Jo Shinonome <jo.shinonome@gmail.com>
2021-05-02 10:35:49 +0900

    update deps

--- update_assets.sh
+++ update_assets.sh
@@ -10,6 +10,7 @@ cp node_modules/@finos/perspective-viewer-d3fc/dist/umd/perspective-viewer-d3fc.
 cp node_modules/@finos/perspective-viewer-datagrid/dist/umd/perspective-viewer-datagrid.js $jsDir
 cp node_modules/dayjs/dayjs.min.js $jsDir
 cp node_modules/dayjs/plugin/utc.js $jsDir/plugin
+cp node_modules/@finos/perspective-viewer/dist/umd/perspective_vieux_bg.wasm $jsDir
 
 # update js for view
 cp node_modules/ag-grid-community/dist/ag-grid-community.min.js assets/view/js/
