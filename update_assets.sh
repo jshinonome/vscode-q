e3b0e5edeb408a554283f72b14a96eab55cd80f0
Jo Shinonome <jo.shinonome@gmail.com>
2022-01-02 13:22:23 +0900

    upgrade perspective

--- update_assets.sh
+++ update_assets.sh
@@ -3,16 +3,13 @@
 # copy perspective package files
 jsDir=assets/query-view/js
 cssDir=assets/query-view/css
-cp node_modules/@finos/perspective/dist/umd/perspective.inline.js $jsDir
+cp node_modules/@finos/perspective/dist/umd/perspective.js $jsDir
 cp node_modules/@finos/perspective-viewer/dist/umd/material*css $cssDir
 cp node_modules/@finos/perspective-viewer/dist/umd/perspective-viewer.js $jsDir
-cp node_modules/@finos/perspective-viewer/dist/umd/perspective-viewer-d3fc.js $jsDir
-cp node_modules/@finos/perspective-viewer/dist/umd/perspective-viewer-datagrid.js $jsDir
 cp node_modules/@finos/perspective-viewer-d3fc/dist/umd/perspective-viewer-d3fc.js $jsDir
 cp node_modules/@finos/perspective-viewer-datagrid/dist/umd/perspective-viewer-datagrid.js $jsDir
 cp node_modules/dayjs/dayjs.min.js $jsDir
 cp node_modules/dayjs/plugin/utc.js $jsDir/plugin
-cp node_modules/@finos/perspective-viewer/dist/umd/perspective_viewer_bg.wasm $jsDir
 
 # update js for view
 cp node_modules/ag-grid-community/dist/ag-grid-community.min.js assets/view/js/
